d2212c469b710248cc0e5556df45ab0798b6b98b
Vishwas3000 <timeupvishwa@gmail.com>
2023-05-31 10:29:18 +0530

    init server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config/dist';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync(typeOrmConfigAsync), ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
